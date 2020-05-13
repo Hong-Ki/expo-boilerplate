80d2c99bc3d8114b24ec88a0f473fa9b6d525c5e
Hong-ki <minhk3259@makestar.com>
2020-05-14 01:37:25 +0900

    change App.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import styled, { ThemeProvider } from "styled-components/native";
+import themes, { Theme, Themes } from "./constants/theme";
+import { useBrowserTheme } from "./hooks/useBrowserTheme";
 
+const StyledRootView = styled.View`
+  ${({ theme }) => {
+    const {
+      colorSet: { background, text },
+    } = theme as Theme;
+    return `
+    flex:1;
+    align-items:center;
+    justify-content:center;
+    background-color: ${background};
+  }
+  `;
+  }}
+`;
 export default function App() {
+  const browserTheme = useBrowserTheme();
+  const [theme, setTheme] = useState<Themes>(browserTheme);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ThemeProvider
+      theme={themes[theme] || themes[browserTheme] || themes.light}
+    >
+      <StyledRootView>
+        <Text>Hello! Miong's boilerplate</Text>
+      </StyledRootView>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
